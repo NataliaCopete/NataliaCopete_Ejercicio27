3bc6e1d58e642f6a47df4d8f9e94d005f7996819
NataliaCopete <37335452+NataliaCopete@users.noreply.github.com>
2018-11-14 08:02:13 -0500

    Update sample.c

--- sample.c
+++ sample.c
@@ -35,3 +35,51 @@ double sigma=atof(argv[3]);
 double fMin=mu-4*sigma;
 double fMax=mu+4*sigma;
 /*double max=gauss(mu,mu,sigma);*/
+for(int i=0; i<size;i++){
+int n2=rank;
+double* gaussiana=new double[N];
+int cont=0;
+gaussiana[0]=mu-sigma;
+
+while(cont<N){
+
+double f=(double)rand() / (double)RAND_MAX;
+double propuesta=fMin + f * (fMax - fMin);
+double F=gauss(propuesta,mu,sigma)/gauss(gaussiana[cont],mu,sigma);
+if (F>1){
+gaussiana[cont+1]=propuesta;
+cont=cont+1;
+
+}
+
+else{
+double r=(double)rand() / (double)RAND_MAX;
+if(r<F){
+
+gaussiana[cont+1]=propuesta;
+cont=cont+1;
+}
+
+else{
+gaussiana[cont+1]=gaussiana[cont];
+cont=cont+1;
+}
+}
+}
+char buffer[50];
+for (int i=0; i<N;i++){
+ofstream (cuenta);
+stringstream s1;
+sprintf(buffer,"%d",rank);
+s1<<buffer;
+string s;
+s1>>s;
+string cadena= "sample"+s+".dat";
+cuenta.open(cadena.c_str());
+cuenta<<gaussiana[i]<<endl;
+cuenta.close();
+}
+}
+MPI_Finalize();
+return 0;
+}
